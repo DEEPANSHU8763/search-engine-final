26c5fbe851a896c18abf48ef813a2582cee9e739
DEEPANSHU <93826687+DEEPANSHU8763@users.noreply.github.com>
2026-03-09 22:55:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from langchain_groq import ChatGroq
 from langchain_community.utilities import ArxivAPIWrapper,WikipediaAPIWrapper
 from langchain_community.tools import ArxivQueryRun,WikipediaQueryRun,DuckDuckGoSearchRun,DuckDuckGoSearchResults
-from langchain.agents import initialize_agent
+from langchain.agents.initialize import initialize_agent
 from langchain.agents.agent_types import AgentType
 from langchain.callbacks import StreamlitCallbackHandler
 import os
